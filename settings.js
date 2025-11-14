1d0a225d6926f57bf319d15447a21ab8bb263325
abc-bitu <dilshandanuka377@gmail.com>
2025-11-14 12:32:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VI5HSI6L#TVra0ci0zSinIJ89Q-wVU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VI5HSI6L#TVra0ciOQOlZlR4y95GItUJOn3ivo0zSinIJ89Q-wVU' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
